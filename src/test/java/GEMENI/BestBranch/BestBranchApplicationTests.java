f5ac5e97a1470dfdd86ea713bc2d28f807ef2ba3
Copteez <copterkpd45@gmail.com>
2024-04-14 20:28:54 +0700

    Initial commit

---
+++ src/test/java/GEMENI/BestBranch/BestBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package GEMENI.BestBranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
