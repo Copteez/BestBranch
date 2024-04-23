c4698673be451578ef36b9d4a18c970a4accf139
Beenayaknon <su.beepak@gmail.com>
2024-04-23 22:38:35 +0700

    Add number of plans in dashboard

--- src/main/java/Demo/DemoController.java
+++ src/main/java/Demo/DemoController.java
@@ -15,7 +15,27 @@ public class DemoController {
     public String dashboard(Model model) {
         OCS o = new OCS();
         System.out.println(o.getAllSciencePlans());
+        // get plan list
         model.addAttribute("plans", o.getAllSciencePlans());
+
+        // get #all sciplan
+        model.addAttribute("totalPlans", o.getAllSciencePlans().size());
+
+        // get #draft sciplan & #submitted sciplan
+        Integer draftPlan = 0;
+        Integer submittedPlan = 0;
+        ArrayList<SciencePlan> AllSciencePlans = o.getAllSciencePlans();
+        for (SciencePlan sp:AllSciencePlans){
+            if (sp.getStatus().equals(SciencePlan.STATUS.SAVED)){
+                draftPlan+=1;
+            }
+            if (sp.getStatus().equals(SciencePlan.STATUS.SUBMITTED)){
+                submittedPlan+=1;
+            }
+        }
+        model.addAttribute("draftPlan", draftPlan);
+        model.addAttribute("submittedPlan", submittedPlan);
+
         return "Dashboard";
     }
 
@@ -33,5 +53,7 @@ public class DemoController {
         }
         return null;
     }
+
+
 }
 
